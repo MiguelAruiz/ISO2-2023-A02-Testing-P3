55f86f52cd371cd2f6d95455f37ce9285cb326be
ManuehGitHub <manuel.perales@alu.uclm.es>
2023-12-06 14:51:59 +0100

    Pom.xml preparation
    ADDITS: none
    
    MODIFS: pom.xml re-structured for testing purposes
    
    REMOVS: none

--- Gravity/src/test/java/ISOA02/Gravity/AppTest.java
+++ Gravity/src/test/java/ISOA02/Gravity/AppTest.java
@@ -1,13 +1,9 @@
 package ISOA02.Gravity;
 
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 public class AppTest {
     
-  @Test
   public void shouldAnswerWithTrue() {
-    assertTrue(true);
+	  
   }
 }
