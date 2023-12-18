0e2c29c530bd75cddf243f730d504e9548773345
MiguelAruiz <96320400+MiguelAruiz@users.noreply.github.com>
2023-12-18 19:52:54 +0100

    v1.0.2

--- Gravity/src/main/java/ISOA02/Gravity/App.java
+++ Gravity/src/main/java/ISOA02/Gravity/App.java
@@ -9,21 +9,21 @@ public class App {
 	  double m1 = 0;
 	  double m2 = 0;
 	  double d = 0;
+	  double force = 0;
+	  gravity_problem gv;
 	  
 	  m1=Read.read_double("Introduce the mass value for Body1 (Kg):");
 	  m2=Read.read_double("Introduce the mass value for Body2 (Kg):");
 	  d=Read.read_double("Introduce the distance value between Body1 and Body2 (Meters):");
 
-	  
-	  gravity_problem gv=new gravity_problem(m1,m2,d);
 	  try {
-		  double force = gv.calculate_force();
+		  gv=new gravity_problem(m1,m2,d);
+		  force = gv.calculate_force();
 		  System.out.print("The total force is "+force+ " Newtons.");
 	  }catch(DividedByZeroException e) {
 		  System.out.print("DividedByZeroException: D cannot be zero.");
 	  }catch(InvalidValueException e) {
 		  System.out.print("InvalidValueException: no value can be negative.");
-	  }
-	  
+	  }    
   }
 }
--- Gravity/src/main/java/ISOA02/Gravity/gravity_problem.java
+++ Gravity/src/main/java/ISOA02/Gravity/gravity_problem.java
@@ -8,7 +8,7 @@ public class gravity_problem {
 	
 	final private double G=6.67*Math.pow(10, -11);
 	
-	public gravity_problem(double m1, double m2, double d) {
+	public gravity_problem(double m1, double m2, double d) throws DividedByZeroException, InvalidValueException {
 		this.setBodyMass1(m1);
 		this.setBodyMass2(m2);
 		this.setDistance(d);
@@ -26,25 +26,31 @@ public class gravity_problem {
 		return this.d;
 	}
 	
-	public void setBodyMass1(double newMass1) {
+	public void setBodyMass1(double newMass1) throws InvalidValueException {
+		if(newMass1 < 0) {
+			throw new InvalidValueException();
+		}
 		this.m1 = newMass1;
 	}
 	
-	public void setBodyMass2(double newMass2) {
+	public void setBodyMass2(double newMass2) throws InvalidValueException{
+		if(newMass2 < 0) {
+			throw new InvalidValueException();
+		}
 		this.m2 = newMass2;
 	}
 	
-	public void setDistance(double newDistance) {
+	public void setDistance(double newDistance) throws DividedByZeroException, InvalidValueException{
+		if(newDistance == 0) {
+			throw new DividedByZeroException();
+		}else if(newDistance < 0) {
+			throw new InvalidValueException();
+		}
+		
 		this.d = newDistance;
 	}
 	
-	public double calculate_force() throws DividedByZeroException, InvalidValueException{
-		if(d==0) {
-			throw new DividedByZeroException();
-		}
-		if(d<0 || m2<=0 || m1<=0) {
-			throw new InvalidValueException();
-		}
+	public double calculate_force() {
 		return (G*this.m1*this.m2)/(this.d*this.d);
 	}
 	
